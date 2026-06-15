c3559c725f4a7fdaec087c4dcc3b2592a90b1629
chagermana <manvinchager28@gmail.com>
2026-06-15 10:31:12 +0300

    Added role-based user structure (system_admin, hospital_admin, healthcare_worker) Added hospital_id foreign key to associate users with hospitals Added ward_id foreign key to associate healthcare workers with wards Supports the simplified ERD using a single users table instead of separate admin and healthcare worker tables

---
+++ database/migrations/2026_06_15_072657_add_hospital_and_ward_to_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+                $table->foreignId('hospital_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+                $table->foreignId('ward_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+
+
+        $table->dropForeign(['hospital_id']);
+        $table->dropForeign(['ward_id']);
+
+        $table->dropColumn('hospital_id');
+        $table->dropColumn('ward_id');
+
+        });
+    }
+};
